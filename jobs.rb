1730815362b4e82e4403b2ed01e19ccf279ea4fb
michaelporter <michaelporter.0988@gmail.com>
2013-05-10 20:10:13 -0400

    moves files into lib dir

--- jobs.rb
+++ jobs.rb
@@ -1,15 +1,14 @@
 #!/usr/bin/env ruby
 
-require_relative 'mechanize_controller.rb'
-require_relative 'job_aggregator.rb'
-require_relative 'made_in_nyc.rb'
+require_relative 'lib/mechanize_controller.rb'
+require_relative 'lib/job_aggregator.rb'
+require_relative 'lib/made_in_nyc.rb'
 
 job_aggregators = []
 job_aggregators << MadeInNyc.new
 
 jobs = []
 job_aggregators.each do |aggregator|
-  puts aggregator.inspect
   found_jobs = aggregator.find_jobs
   jobs += found_jobs
 end
