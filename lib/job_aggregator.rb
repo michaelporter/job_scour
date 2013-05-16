f73f788971c920aa2ad675120b527cdcd314f25c
michaelporter <michaelporter.0988@gmail.com>
2013-05-15 21:20:26 -0400

    fixes reference to browser file; small refactors of aggregator

--- lib/job_aggregator.rb
+++ lib/job_aggregator.rb
@@ -6,68 +6,111 @@ class JobAggregator
     @found_jobs = []
 
     @keywords ||= ['ruby', 'rails']
+    @no_result_pages = []
 
     @path_actions = ['jobs', 'careers', 'about', 'team']
+    @pages_checked = []
     @url ||= ''
+  end
+
+  def get_unique_links(links)
+    links.dup.keep_if {|link| link.text != "(hiring)"}
   end
 
   def find_jobs
     @browser.new_page(@url) do |page|
-      progress_bar = new_progressbar(:title => page.title, :total => page.links.length)
+      links = page.links
+      unique_links = get_unique_links(links)
+      progress_bar = new_progressbar(:title => page.title, :total => unique_links.length)
 
-      page.links.shuffle.each do |link|
+      links.each_with_index do |link, index|
 
-        @job_found= false
-        create_links(link.href) do |composed_link|
-          begin
-            scour(composed_link)
-          rescue
-            next
+        @job_found = false
+        unless index == links.length - 1 || links[index + 1].text == "(hiring)"
+          create_links(link.href) do |composed_link|
+            begin
+              unless @job_found
+                scour(composed_link)
+              end
+            rescue
+              next
+            end
           end
-        end
 
-        progress_bar.increment
+          progress_bar.increment
+        end
       end
     end
     
+    puts @found_jobs.split(",\n")
+
+    File.open("results.txt", "w") do |f|
+      f.write @found_jobs.split(",\n")
+    end
+
     @found_jobs
   end
 
   private
 
-  def create_links(url, &block)
-    unless url_invalid?(url)
+  def collect_links_to_try(url)
+    try_links = []
+
+    if url =~ /\w+\/\w+/
+      try_links << url
+    else
       @path_actions.each do |path_action|
         path_action = "/" + path_action unless url[-1] == '/'
-        composed_link = url + path_action
-
-        unless url_invalid?(composed_link)
-          yield composed_link if block_given?
-        end
+        try_links << url + path_action
       end
     end
+
+    try_links
+  end
+
+  def create_links(url, &block)
+    unless url_invalid?(url)
+      try_links = collect_links_to_try(url)
+
+      not_found_in = 0
+      try_links.each do |try_link|
+        unless url_invalid?(try_link)
+          yield try_link if block_given?
+
+          not_found_in += 1 unless @job_found
+        end
+      end
+
+      if not_found_in == try_links.length - 1
+        @no_result_pages << url
+      end
+    end
+  end
+
+  def keyword_regex
+    /#{Regexp.quote(keywords.join("|"))}/i
   end
 
   def new_progressbar(options = {})
     total = options[:total] || 100
     title = options[:title] || ''
+    format = options[:format] || "%t |%B| %p%%"
 
-    ProgressBar.create(:total => total, :title => title, :length => 80)
+    ProgressBar.create(:total => total, :title => title, :length => 80, :format => format)
   end
 
   def scour(job_link)
-    unless @job_found
-      @browser.new_page(job_link) do |jobs_page|
-        if jobs_page.body =~ keyword_regex
-          @found_jobs << job_link
-          @job_found = true
-        end
+    @browser.new_page(job_link) do |jobs_page|
+      @pages_checked << jobs_page
+      if jobs_page.body =~ keyword_regex
+        @found_jobs << job_link
+        @job_found = true
       end
     end
   end
 
   def url_invalid?(url)
-    url =~ /mailto|#{Regexp.quote(@url)}/i
+    url =~ /mailto|#{Regexp.quote(@url)}/i || !(url =~ /http(s)?\:\/\//)
   end
 end
 
